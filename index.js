93f97565c54f275bd11410cb8ad34e0390f4c2e5
samiulbasirfahim <samiulbasirfahim@hotmail.com>
2022-04-24 21:42:10 +0600

    cart data loaded

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express")
-const { MongoClient, ServerApiVersion } = require("mongodb")
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb")
 require("dotenv").config()
 const cors = require("cors")
 const port = process.env.PORT || 4000
 const app = express()
 app.use(cors())
-app.use(express())
+app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.9iutd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const client = new MongoClient(uri, {
@@ -21,18 +21,32 @@ const run = async () => {
 			const {
 				query: { page, productCount },
 			} = req
-            const cursor = await productsCollection.find({})
+			const cursor = await productsCollection.find({})
 
-            if(page && productCount){
-                const product = await cursor.skip(+page * +productCount).limit(+productCount).toArray()
-                res.send(product)
-            }
+			if (page && productCount) {
+				const product = await cursor
+					.skip(+page * +productCount)
+					.limit(+productCount)
+					.toArray()
+				res.send(product)
+			} else {
+				const product = await cursor.toArray()
+				res.send(product)
+			}
 		})
-        app.get('/productCount',async (req, res)=>{
-            const count = await productsCollection.countDocuments()
-            console.log(count);
-            res.send({count})
-        })
+		app.get("/productCount", async (req, res) => {
+			const count = await productsCollection.countDocuments()
+			// console.log(count)
+			res.send({ count })
+		})
+		app.post('/productsById', async (req, res) => {
+			const keys = req.body
+			const ids = keys.map( key => ObjectId(key) )
+			const query = { _id:{$in: ids}}
+			const cursor = productsCollection.find(query)
+			const result = await cursor.toArray()
+			res.send(result)
+		})
 	} finally {
 		// console.log('final');
 	}
